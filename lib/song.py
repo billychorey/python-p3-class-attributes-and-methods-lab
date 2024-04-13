66ca88ec958ce22041449e49126623ed5e282771
Billy <billychorey@gmail.com>
2024-04-13 18:36:09 -0400

    attributes and methods lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {} 
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment the count each time a new song is created
+        Song.count += 1
+        
+        # Add the genre to the set of genres
+        Song.genres.add(genre)
+        
+        # Add the artist to the set of artists
+        Song.artists.add(artist)
+        
+        # Update the genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        
+        # Update the artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
